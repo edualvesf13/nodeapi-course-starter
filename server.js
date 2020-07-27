e25c2b9cd1a544f5a1b2a8996e1500ee4abc6dba
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-07-27 10:10:03 -0300

    Testing the connection to the BD by inserting a test product.

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Starting the app
 const app = express()
 
 // Starting the DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useUnifiedTopology: true, useNewUrlParser: true})
-require('./src/models/Product')
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
 
 // Creating the route.
 app.get('/', (req, res) => {
-    res.send('Hello, servidor!')
+
+    Product.create({
+        title: 'React Native',
+        descriptions: 'Build native apps with React Native',
+        url: 'https://github.com/facebook/react-native'
+    })
+
+    return res.send('Hello, servidor!')
 })
 
 app.listen(3001)
