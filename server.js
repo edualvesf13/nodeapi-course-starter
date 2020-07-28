0951b554e4e2a52ad509bb996ad4447973b5f803
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-07-28 12:38:20 -0300

    Creation of register.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir')
 
 // Starting the app
 const app = express()
+app.use(express.json())
 
 // Starting the DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useUnifiedTopology: true, useNewUrlParser: true})
